34de971be0ecb3ffb64244a80633ea0977f316af
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-10-06 13:13:18 -0400

    ReadMe additions

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+//HTML routes
+
 app.get("/", (req,res) =>{
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -23,7 +26,38 @@ app.get("/exercise", (req,res) =>{
     res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+//API routes
+
+app.get("/api/workouts", (req,res) =>{
+    //send an array of objects as JSON
+
+});
+
+app.put("/api/workouts/:id", (req,res) =>{
+    const workoutid = req.params.id;
+    const exercise = req.body;
+    // send information??? as JSON  ||  Anything sent is completely ignored!
+});
+
+app.post("/api/workouts", (req, res) =>{
+    const workout = req.body; // As written front-end will always send an EMPTY object in Req.Body!!
+    // send information??? as JSON
+});
+
+app.get("/api/workouts/range", (req, res) =>{
+    // send JSON potentially array
+});
+
+
+
+
+
+
+
+
 app.listen(port, ()=>{
     console.log(`Server is listening on ${port}!`);
 });
+
+
 
---
+++ README.md
@@ -0,0 +1,19 @@
+# Fitness-Tracker
+A personal fitness tracker application that allows the user to record their workout sessions. Features a Mongo database w/ a mongoose schema and handle routes with Express.
+
+
+# Technologies Utilized
+
+* HTML
+* CSS
+* Javascript
+* Express
+* MongoDB
+* Mongoose
+* NPM modules 
+
+# Links
+
+Link to deployed application on Heroku
+
+Link to my [GitHub repository](https://github.com/LukeMcM89/Fitness-Tracker).
