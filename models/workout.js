07778afc72c9c2232ca1b2f4ea5acfd75a9c71b1
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-10-07 14:22:33 -0400

    Mongoose fitness Schema

--- models/workout.js
+++ models/workout.js
@@ -1,6 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema;
+const fitnessSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Select an exercise.",
+      },
+      name: {
+        type: String,
+        required: "Select an exercise name.",
+      },
+      duration: {
+        type: Number,
+        required: "Select a amount of time.",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
 
-const FitnessSchema = new Schema ({});
+const Workout = mongoose.model("Workout", fitnessSchema);
+
+module.exports = workout;
